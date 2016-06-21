dbc0d3c23559e81fff1422f6b2440f46d5890d08
gandhalis <gandhalisamant@gmail.com>
2016-06-21 15:44:16 +0530

    printing log

--- server.js
+++ server.js
@@ -3,10 +3,26 @@ var builder = require('botbuilder');
 
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot({ appId: process.env.APP_ID, appSecret: process.env.APP_SECRET });
+// bot.add('/', function (session) {
+//     session.send('Hello World');
+// });
 bot.add('/', function (session) {
-    session.send('Hello World');
+    console.log(session);
+    if (!session.userData.name) {
+        session.beginDialog('/profile');
+    } else {
+        session.send('Hello %s!', session.userData.name);
+    }
 });
-
+bot.add('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
 // Setup Restify Server
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
