154411a338396442e71b3c2c40cd5f91406f16cb
manisbindra <maninder.bindra@gmail.com>
2016-07-11 17:48:57 +0530

    added LUIS dialog code

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+// bot.dialog('/', function (session) {
+//     session.send("Hello World");
+// });
 
 
 // var restify = require('restify');
@@ -36,46 +36,46 @@ bot.dialog('/', function (session) {
 // var bot = new builder.BotConnectorBot({ appId: process.env.APP_ID, appPassword: process.env.APP_SECRET });
 
 
-// var dialog = new builder.LuisDialog(process.env.LUIS_URL    );
-// bot.add('/', dialog);
+var dialog = new builder.LuisDialog(process.env.LUIS_URL    );
+bot.dialog('/', dialog);
 
-// dialog.on('ShowReportTypes', [
-//     function (session, args) {
+dialog.on('ShowReportTypes', [
+    function (session, args) {
        
-//         // builder.Message.text(session, "You can generate the following reports : 1,2,3"); //values to be read from JSON and shown
-//         session.send("You can generate the following reports : 1,2,3");
+        // builder.Message.text(session, "You can generate the following reports : 1,2,3"); //values to be read from JSON and shown
+        session.send("You can generate the following reports : 1,2,3");
        
-//     }
-// ]);
+    }
+]);
 
-// dialog.on('Greeting', [
-//     function (session, args) {
+dialog.on('Greeting', [
+    function (session, args) {
        
-//         // builder.Prompts.text(session, "Hello There! How may I help you, I can help you in viewing available report types, and in requesting generation or particular reports"); //values to be read from JSON and shown
-//         // builder.Message.text(session, "Hello There! How may I help you, I can help you in viewing available report types, and in requesting generation or particular reports");
-//        session.send("Hello There! How may I help you, I can help you in viewing available report types, and in requesting generation or particular reports");
-//     }
-// ]);
+        // builder.Prompts.text(session, "Hello There! How may I help you, I can help you in viewing available report types, and in requesting generation or particular reports"); //values to be read from JSON and shown
+        // builder.Message.text(session, "Hello There! How may I help you, I can help you in viewing available report types, and in requesting generation or particular reports");
+       session.send("Hello There! How may I help you, I can help you in viewing available report types, and in requesting generation or particular reports");
+    }
+]);
 
-// dialog.on('GenerateReport', [
-//     function (session, args) {
+dialog.on('GenerateReport', [
+    function (session, args) {
        
-//         var reportType = builder.EntityRecognizer.findEntity(args.entities, 'ReportType');
-//         if (!reportType) {
-//             // builder.Message.text(session, "Could not Identify which report you want to generate");
-//             session.send("Could not Identify which report you want to generate");
+        var reportType = builder.EntityRecognizer.findEntity(args.entities, 'ReportType');
+        if (!reportType) {
+            // builder.Message.text(session, "Could not Identify which report you want to generate");
+            session.send("Could not Identify which report you want to generate");
             
-//         } else {
-//             // next({ response: task.entity });
-//             // builder.Message.text(session, "Do you want to generate the " + reportType.entity + " report");
-//             session.send("Do you want to generate the " + reportType.entity + " report");
-//         }
+        } else {
+            // next({ response: task.entity });
+            // builder.Message.text(session, "Do you want to generate the " + reportType.entity + " report");
+            session.send("Do you want to generate the " + reportType.entity + " report");
+        }
         
        
-//     }
-// ]);
+    }
+]);
 
-// dialog.onDefault(builder.DialogAction.send("I'm sorry. I didn't understand."));
+dialog.onDefault(builder.DialogAction.send("I'm sorry. I didn't understand."));
 
 // var server = restify.createServer();
 // server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
