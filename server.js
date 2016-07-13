d4e175f17938e1817f24bc2102eaecd3101a24e0
gandhalis <gandhalisamant@gmail.com>
2016-07-13 16:00:18 +0530

    Added parameter code

--- server.js
+++ server.js
@@ -9,14 +9,6 @@ var bot = new builder.BotConnectorBot({ appId: process.env.APP_ID, appSecret: pr
 var dialog = new builder.LuisDialog(process.env.LUIS_URL);
 bot.add('/', dialog);
 
-dialog.on('ShowReportTypes', [
-    function (session, args) {
-       
-        // builder.Message.text(session, "You can generate the following reports : 1,2,3"); //values to be read from JSON and shown
-        session.send("You can generate the following reports : 1- Profile Report, 2-Trend Report");
-       
-    }
-]);
 
 dialog.on('Greeting', [
     function (session, args) {
@@ -27,20 +19,83 @@ dialog.on('Greeting', [
     }
 ]);
 
-dialog.on('GenerateReport', [
+dialog.on('ShowReportTypes', [
     function (session, args) {
        
+        // builder.Message.text(session, "You can generate the following reports : 1,2,3"); //values to be read from JSON and shown
+        session.send("You can generate the following reports : 1- Profile Report, 2-Trend Report");
+       
+    }
+]);
+
+dialog.on('GenerateReport', [
+    function (session, args, next) {
         var reportType = builder.EntityRecognizer.findEntity(args.entities, 'ReportType');
-        if (!reportType) {
+        var reportParams = session.dialogData.reportParams = {
+          reportType: reportType ? reportType.entity : null,
+          openCount : null,
+          sentCount : null,
+          subjectLine : null,
+          
+        };
+        
+        if (!reportParams.reportType) {
             // builder.Message.text(session, "Could not Identify which report you want to generate");
-            session.send("Could not Identify which report you want to generate");
-            
-        } else {
-            // next({ response: task.entity });
-            // builder.Message.text(session, "Do you want to generate the " + reportType.entity + " report");
-            session.send("Do you want to generate the " + reportType.entity + " report");
+            builder.Prompts.text(session, "Could not Identify Report Name. You can generate the following reports : 1- Profile Report, 2-Trend Report");
+        } 
+        else {
+            next();
+        }
+    },
+    function (session, args, next) {
+        var reportParams = session.dialogData.reportParams;
+        if (reportParams.reportType && !reportParams.openCount) {
+            builder.Prompts.text(session,"Set OpenCount");
+        }
+        else{
+            next();
+        }
+    },
+    function (session, results, next) {
+        var reportParams = session.dialogData.reportParams;
+        if (results.response) {
+           reportParams.openCount = results.response;
+        }
+
+        if (reportParams.reportType && reportParams.openCount && !reportParams.sentCount) {
+            builder.Prompts.text(session,"Set SentCount");
+        }
+        else{
+            next();
+        }
+    },
+    function (session, results, next) {
+        var reportParams = session.dialogData.reportParams;
+        if (results.response) {
+           reportParams.sentCount = results.response;
+        }
+
+        if (reportParams.reportType && reportParams.openCount && reportParams.sentCount && !reportParams.subjectLine) {
+            builder.Prompts.text(session,"Set Subject Line");
+        }
+        else{
+            next();
+        }
+    },
+    function (session, results, next) {
+        var reportParams = session.dialogData.reportParams;
+        if (results.response) {
+           reportParams.subjectLine = results.response;
+        }
+
+        if (reportParams.reportType && reportParams.openCount && reportParams.sentCount && reportParams.subjectLine) {
+            builder.Prompts.text(session,"Creating "+reportParams.reportType+" report with Sent Count-"+reportParams.reportType+", Open Count-"+reportParams.reportType+" and Subject Line-"+reportParams.reportType);
+        }
+        else{
+            next();
         }
     }
+    
 ]);
 
 
