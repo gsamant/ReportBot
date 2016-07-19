9493c7a89162f225705796c6e1a26d40268a87f9
manisbindra <maninder.bindra@gmail.com>
2016-07-19 17:44:03 +0530

    minor change

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ var server = restify.createServer();
 server.post('/api/messages', connector.listen());
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
+    console.log("LUIS URL :" + process.env.LUIS_URL);
 });
 
 
